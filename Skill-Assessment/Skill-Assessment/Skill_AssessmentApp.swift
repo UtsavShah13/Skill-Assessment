0b06b990337db990e9209f1b68d0b5c5ae2a1be9
Utsav <utsavshah211@gmail.com>
2025-01-29 00:08:25 +0530

    Project Created

---
+++ Skill-Assessment/Skill-Assessment/Skill_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skill_AssessmentApp.swift
+//  Skill-Assessment
+//
+//  Created by Utsav Shah on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Skill_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
